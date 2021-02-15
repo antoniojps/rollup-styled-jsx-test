292ca12d99f925bfbaeba2441da13038ba5413d8
antoniojps <antonio.santos@mindera.com>
2021-02-15 12:14:09 +0000

    add basic sass support

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,13 +1,19 @@
 import React from "react";
+import _JSXStyle from "styled-jsx/style";
 
 class HelloWorld extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1 className="hello">
+          Hello <span className="hello__red">World!</span>
+        </h1>
         <style jsx>{`
-          h1 {
-            color: red;
+          .hello {
+            color: pink;
+            &__red {
+              color: red;
+            }
           }
         `}</style>
       </div>
